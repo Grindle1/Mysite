b4ac068462bea529dbe521c4e9b7791f02708c04
Grindle <77505784+Grindle1@users.noreply.github.com>
2024-06-27 13:16:29 +0530

    update2
    123

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,31 +91,25 @@
     var form = $('#contact-form');
 
     var formMessages = $('.form-message');
-    $(form).submit(function (e) {
+    $(form).submit(function(e) {
         e.preventDefault();
         var formData = $(form).serialize();
         $.ajax({
-                type: 'POST',
-                url: $(form).attr('action'),
-                data: formData
-            })
-            .done(function (response) {
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
-                $(formMessages).text(response);
-
-                $('#contact-form input,#contact-form textarea').val('');
-            })
-            .fail(function (data) {
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
-
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                }
-            });
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+        .done(function(response) {
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
+            $(formMessages).text("Thank you! Your message has been sent.");
+            $('#contact-form input,#contact-form textarea').val('');
+        })
+        .fail(function(data) {
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+            $(formMessages).text('Oops! An error occurred and your message could not be sent.');
+        });
     });
     
     
